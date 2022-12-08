3d2a1507ca310913228033ea219c1f44a0340f46
MockeviciusTomas <112574863+MockeviciusTomas@users.noreply.github.com>
2022-12-08 21:04:51 +0200

    Task 8 complete

--- JS Tasks/Task 8/script.js
+++ JS Tasks/Task 8/script.js
@@ -5,3 +5,28 @@ subtraction(a, b) - priima du skaičius ir grąžina jų skirtumą.
 multiplication(a, b) - priima du skaičius ir grąžina jų daugybos rezultatą;
 division(a, b) - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+function Item () {
+    this.sum = function (a, b) {
+        return a + b
+    }
+    
+    this.subtraction = function (a, b) {
+        return a - b
+    }
+    
+    this.multiplication = function (a, b) {
+        return a * b
+    }
+    
+    this.division = function (a, b) {
+        return a / b
+    }
+}
+
+const item = new Item()
+
+console.log(item.sum(4, 8))
+console.log(item.subtraction(4, 8))
+console.log(item.multiplication(4, 8))
+console.log(item.division(4, 8))
